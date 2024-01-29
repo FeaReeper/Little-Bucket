51d99b1da426cc561ec5df040a0bd316faa1a8ae
Matthew Reep <reepmatt20@gmail.com>
2024-01-29 17:13:31 -0500

    vercel changes

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cors(
 ))
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://little-bucket-frontend.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
