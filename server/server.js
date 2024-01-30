068a79e7386b80dcaa138fe586f09044cf5a687c
Matthew Reep <reepmatt20@gmail.com>
2024-01-29 19:01:33 -0500

    vercel

--- server/server.js
+++ server/server.js
@@ -14,16 +14,9 @@ app.use(cors(
     {
         credentials:true, 
         methods: ["POST", "GET"], 
-        origin: ["*"]
+        origin: "*"
     }
 ))
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-})
 
 app.use(express.static('public'))
 
