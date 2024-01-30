aa4259657fd12e52b82bc52a00fae2fc30866028
Matthew Reep <reepmatt20@gmail.com>
2024-01-30 16:02:52 -0500

    vercel

--- server/server.js
+++ server/server.js
@@ -20,10 +20,6 @@ app.use(cors(
 
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-    res.json("Hello")
-})
-
 const AllUserRoutes = require("./routes/user.routes");
 AllUserRoutes(app);
 
