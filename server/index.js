1e6bee667670411aba6d64d71b1ad0cbb2e209d4
Matthew Reep <reepmatt20@gmail.com>
2024-02-03 13:54:16 -0500

    vercel

--- server/index.js
+++ server/index.js
@@ -1,36 +1,39 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
-const cookieParser = require('cookie-parser')
-const path = require('path');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-
-require("dotenv").config()
+require("dotenv").config();
 require("./config/mongoose.config");
-    
+
 app.use(express.json(), express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(cors(
-    {
-        credentials: true, 
-        methods: ["POST", "GET"],
-        origin: ["https://little-bucket.vercel.app"]
-    }
-))
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    methods: ["POST", "GET"],
+    origin: ["https://little-bucket.vercel.app"],
+  })
+);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.json("Hello");
-})
+  res.json("Hello");
+});
 const AllUserRoutes = require("./routes/user.routes");
 AllUserRoutes(app);
 
-const AllBucketRoutes = require("./routes/bucket.routes")
-AllBucketRoutes(app)
+const AllBucketRoutes = require("./routes/bucket.routes");
+AllBucketRoutes(app);
 
-const AllKidRoutes = require('./routes/kid.routes')
-AllKidRoutes(app)
-    
+const AllKidRoutes = require("./routes/kid.routes");
+AllKidRoutes(app);
+
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
