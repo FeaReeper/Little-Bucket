7973a3365ec8b2d37a2db80f3c306b00d41c7268
Matthew Reep <reepmatt20@gmail.com>
2024-02-14 20:33:42 -0500

    multer destination change

--- server/routes/kid.routes.js
+++ server/routes/kid.routes.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images');
+        cb(null, './src/assets');
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + "--" + file.originalname);
